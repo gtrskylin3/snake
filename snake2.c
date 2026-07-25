120c365cd29f8f019752ff9d7fc406d7b11d2f4e
danek <danek@w.w>
2026-07-26 01:07:56 +0700

    snake final

--- snake2.c
+++ snake2.c
@@ -2,9 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-#include <windows.h>
+#include <unistd.h>
+#include <ncurses.h>
 #define height 20
-#define width 40
+#define width 50
+#define MAX_SNAKE 100
+
+typedef struct
+{
+    int x;
+    int y;
+} Point;
+
+typedef enum {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+} Directions;
 
 void draw_game_location(char game_map[height][width])
 {
@@ -13,9 +28,24 @@ void draw_game_location(char game_map[height][width])
 
         for (int j = 0; j < width; ++j)
         {
-            printf("%c", game_map[i][j]);
+            char symbol = game_map[i][j];
+
+            if (symbol == '#') {
+                attron(COLOR_PAIR(1)); // Зеленый для стен
+                mvaddch(i, j, symbol);
+                attroff(COLOR_PAIR(1));
+            } else if (symbol == '@') {
+                attron(COLOR_PAIR(2)); // Синий для змейки
+                mvaddch(i, j, symbol);
+                attroff(COLOR_PAIR(2));
+            } else if (symbol == '$') {
+                attron(COLOR_PAIR(3)); // Красный для еды
+                mvaddch(i, j, symbol);
+                attroff(COLOR_PAIR(3));
+            } else {
+                mvaddch(i, j, ' ');
+            }
         }
-        printf("\n");
     }
 }
 
@@ -25,7 +55,7 @@ void create_game_location(char game_map[height][width])
     {
         for (int j = 0; j < width; ++j)
         {
-            game_map[i][j] = '.';
+            game_map[i][j] = ' ';
         }
     }
 
@@ -41,30 +71,56 @@ void create_game_location(char game_map[height][width])
     }
 }
 
-typedef struct
-{
-    int x;
-    int y;
-} Point;
-
-void move_snake(int snake_length, Point snake[snake_length])
+void move_snake(int snake_length, Point snake[], Directions dir)
 {
     for (int i = snake_length - 1; i > 0; i--)
     {
-        snake[i].x = snake[i - 1].x;
-        snake[i].y = snake[i - 1].y;
+        snake[i] = snake[i-1];
     }
-    snake[0].x++;
-    if (snake[0].x > width - 2)
+    switch (dir)
     {
-        snake[0].x = 1;
+        case UP: snake[0].y--; break;
+        case DOWN: snake[0].y++; break;
+        case LEFT: snake[0].x--; break;
+        case RIGHT: snake[0].x++; break;
     }
 }
 
-void init_snake(int snake_length, Point snake[snake_length])
+bool collision_check(int snake_length, Point snake[]){
+    if (snake[0].x <= 0 || snake[0].x >= width - 1 || 
+        snake[0].y <= 0 || snake[0].y >= height - 1) {
+        return true;
+    }
+    for (int i = 1; i < snake_length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void handle_input(Directions *dir) {
+    int ch = getch();
+    switch (ch)
+    {
+    case 'w': case 'W': case KEY_UP:
+        if (*dir != DOWN) *dir = UP;
+        break;
+    case 's': case 'S': case KEY_DOWN:
+        if (*dir != UP) *dir = DOWN;
+        break;
+    case 'a': case 'A': case KEY_LEFT:
+        if (*dir != RIGHT) *dir = LEFT;
+        break;
+    case 'd': case 'D': case KEY_RIGHT:
+        if (*dir != LEFT) *dir = RIGHT;
+        break;
+    }
+}
+void init_snake(int snake_length, Point snake[])
 {
-    snake[0].y = rand() % (height - 2) + 1;
-    snake[0].x = rand() % (width - 2) + 1;
+    snake[0].y = height / 2;
+    snake[0].x = width / 2;
     for (int i = 1; i < snake_length; ++i)
     {
         snake[i].y = snake[i - 1].y;
@@ -72,52 +128,94 @@ void init_snake(int snake_length, Point snake[snake_length])
     }
 }
 
-void draw_snake(char game_map[height][width], int snake_length, Point snake[snake_length])
+void draw_snake(char game_map[height][width], int snake_length, Point snake[])
 {
     for (int i = 0; i < snake_length; ++i)
     {
-        game_map[snake[i].y][snake[i].x] = 'o';
+        game_map[snake[i].y][snake[i].x] = '@';
     }
 }
 
-void cursor_fix()
-{
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO cursorInfo;
-
-    GetConsoleCursorInfo(console, &cursorInfo);
-    cursorInfo.bVisible = FALSE;
-    SetConsoleCursorInfo(console, &cursorInfo);
+void draw_score(int snake_length){
+    attron(COLOR_PAIR(4) | A_BOLD); // Желтый жирный текст
+    mvprintw(0, 2, " SCORE: %d ", (snake_length - 3) * 10);
+    attroff(COLOR_PAIR(4) | A_BOLD);
 }
 
-void clear_screen()
-{
-    COORD cursorPosition;
-    cursorPosition.X = 0;
-    cursorPosition.Y = 0;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+void draw_food(char game_map[height][width], Point food){
+    game_map[food.y][food.x] = '$';
 }
-void render(char game_map[height][width])
+void render(Point food, int snake_length, Point snake[], char game_map[height][width])
 {
-    clear_screen();
+    draw_snake(game_map, snake_length, snake);
+    draw_food(game_map, food);
+    clear();
     draw_game_location(game_map);
-    Sleep(80);
+    draw_score(snake_length);
+    refresh();
+    usleep(80000);
+}
+
+void spawn_food(Point *food, int snake_length, Point snake[]){
+    bool on_snake;
+    do {
+        on_snake = false;
+        food->x = rand() % (width - 2) + 1;
+        food->y = rand() % (height - 2) + 1;
+        for (int i = 0; i < snake_length; ++i){
+            if (snake[i].x == food->x && snake[i].y == food->y){
+                on_snake = true;
+                break;
+            }
+        }
+    } while (on_snake);
+}
+
+
+bool check_food(int snake_length, Point snake[], Point food){
+    return (snake[0].x == food.x && snake[0].y == food.y);
 }
 
 int main()
 {
-    cursor_fix();
-    int snake_length = 3;
+    initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    if (has_colors()){
+        start_color();
+        use_default_colors();
+        init_pair(1, COLOR_WHITE, -1);
+        init_pair(2, COLOR_GREEN, -1);
+        init_pair(3, COLOR_RED, -1);
+        init_pair(4, COLOR_YELLOW, -1);
+    }
     srand(time(NULL));
+    int snake_length = 3;
     char game_map[height][width];
-    Point snake[snake_length];
+    Point snake[MAX_SNAKE];
+    Point food;
+    Directions dir = RIGHT;
     init_snake(snake_length, snake);
-    system("cls");
+    system("clear");
+    spawn_food(&food, snake_length, snake);
     while (true)
     {
         create_game_location(game_map);
-        move_snake(snake_length, snake);
-        draw_snake(game_map, snake_length, snake);
-        render(game_map);
+        handle_input(&dir);
+        move_snake(snake_length, snake, dir);
+        if (collision_check(snake_length, snake)){
+            break;
+        }
+        if (check_food(snake_length, snake, food)){
+            if (snake_length < MAX_SNAKE) snake_length++;
+            spawn_food(&food, snake_length, snake);
+        }
+        render(food, snake_length, snake, game_map);
     }
+    endwin();
+    printf("Game Over! Вы пососали!\n");
+    return 0;
 }
